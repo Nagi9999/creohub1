9da194667c68d007f08d068864e7d0c5e47e8c67
Naji <najielhakim9@gmail.com>
2023-09-21 12:39:20 +0300

    Project setup

--- src/app/components/text.js
+++ src/app/components/text.js
@@ -1,7 +1,7 @@
 
   export default function Text({  children }) {
     return (
-        <div className="text-gray-700 text-lg italic font-semibold text-center indent-4 text-orange-950 bg-amber-100 border-4 border-slate-950 my-16 mx-48 p-8">
+        <div className=" text-lg italic font-semibold text-center indent-4 text-orange-950 bg-amber-100 border-4 border-slate-950 my-16 mx-48 p-8">
         {children}
       </div>
       
